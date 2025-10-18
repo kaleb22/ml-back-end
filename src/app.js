c1945d40ffbd82fc7ecb2619bb23aa6669941cc7
Kaleb Dalla Pria <kaleb22@hotmail.com>
2025-10-17 22:33:50 -0300

    feat: impl api endpoints

--- src/app.js
+++ src/app.js
@@ -1,9 +1,72 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/items', (req, res) => {
+  const searchTerm = req.query.search;
+  console.log(searchTerm)
+  fs.readFile('./src/search.json', 'utf-8', (error, json) => {
+    if(error) {
+      console.error('Error reading file:', error);
+      return res.status(500).send('Error reading data file.');
+    }
+
+    try {
+      const data = JSON.parse(json);
+      res.json(data);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+      res.status(500).send('Error parsing JSON data.');
+    }
+  })
+})
+
+app.get('/api/items/:id', (req, res) => {
+  const id = req.params.id;
+  let items;
+  let descriptions;
+
+  fs.readFile('./src/items.json', 'utf-8', (error, json) => {
+    if(error) {
+      console.error('Error reading file:', error);
+      return res.status(500).send('Error reading data file.');
+    }
+
+    try {
+      items = JSON.parse(json);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+      res.status(500).send('Error parsing JSON data.');
+    }
+
+    fs.readFile('./src/descriptions.json', 'utf-8', (error, json) => {
+      if(error) {
+        console.error('Error reading file:', error);
+        return res.status(500).send('Error reading data file.');
+      }
+
+      try {
+        descriptions = JSON.parse(json);
+      } catch (parseError) {
+        console.error('Error parsing JSON:', parseError);
+        res.status(500).send('Error parsing JSON data.');
+      }
+
+      let item = items.filter(el => el.id === id);
+      let description = descriptions.filter(el => el.id === id)
+      const response = {
+        author: {
+          name: "Kaleb",
+          lastname: "Dalla"
+        },
+        ...item[0],
+       ...description[0]
+
+      }
+      res.json(response);
+    })
+  })
 })
 
 app.listen(port, () => {
